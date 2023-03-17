1284bcc37f97ef4fb8a302c7f4b9573777836082
ferchoeth <ferchoeth@gmail.com>
2023-03-17 12:12:29 -0600

    repo init

---
+++ src/main/java/com/ferchoeth/demosupermarket/DemoSuperMarketApplication.java
@@ -0,0 +1,13 @@
+package com.ferchoeth.demosupermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSuperMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSuperMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferchoeth/demosupermarket/DemoSuperMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferchoeth.demosupermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSuperMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
